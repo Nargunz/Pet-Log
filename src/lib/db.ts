688f8efa2703dd50eb68f8d3ad71b4cbf44e5a19
JoshTabsDEv <tabura.joshua@cec.edu.ph>
2025-11-17 21:06:43 +0800

    update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,16 +5,23 @@ declare global {
 }
 
 export function getPool(): Pool {
-  const databaseUrl = process.env.DATABASE_URL;
-  console.log(databaseUrl);
+  const host = process.env.MYSQL_HOST;
+  const database = process.env.MYSQL_DATABASE;
+  const user = process.env.MYSQL_USER;
+  const password = process.env.MYSQL_PASSWORD;
+  const port = Number(process.env.MYSQL_PORT || 3306);
 
-  if (!databaseUrl) {
-    throw new Error(databaseUrl);
+  if (!host || !database || !user) {
+    throw new Error("MySQL env variables are not fully set.");
   }
 
   if (!globalThis.mysqlPool) {
     globalThis.mysqlPool = mysql.createPool({
-      uri: databaseUrl,
+      host,
+      database,
+      user,
+      password,
+      port,
       waitForConnections: true,
       connectionLimit: 10,
     });
